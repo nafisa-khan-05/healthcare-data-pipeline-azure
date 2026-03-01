04618e3be4e405163be6f381d89f4f290ad3057c
Nafisa Khan <nafisakhan9699@gmail.com>
2026-03-01 16:25:25 +0530

    bronze_demo.py

--- bronze/bronze_demo.py
+++ bronze/bronze_demo.py
@@ -1,1 +1,15 @@
+from pyspark.sql import SparkSession
 
+# create spark session
+spark = SparkSession.builder.appName("BronzeLayer").getOrCreate()
+
+# Read raw JSON data
+df_raw = spark.read.json("data/patient.json")
+
+# Show Raw data (for testing)
+df_raw.show()
+
+# Write to Bronze Layer (parquet format)
+df_raw.write.mode("overwrite").parquet("bronze/patient")
+
+print("Bronze layer data stored successfully.")
